99b7da16a7c8cb36650ff62b6b6d771a9f8bc188
Joden5316 <138895167+Joden5316@users.noreply.github.com>
2026-04-22 18:09:13 +0800

    Update Calculator Laboratory.js

--- Calculator Laboratory.js
+++ Calculator Laboratory.js
@@ -1,6 +1,8 @@
-// 4: addThreeAndFour function
-function addThreeAndFour() {
-    return 3 + 4;
+// 6–9: reusable function (calculateSum)
+function calculateSum(num1, num2) {
+    return num1 + num2;
 }
-console.log(addThreeAndFour()); // 7
 
+console.log(calculateSum(2, 5));   // 7
+console.log(calculateSum(10, 10)); // 20
+console.log(calculateSum(5, 5));   // 10
